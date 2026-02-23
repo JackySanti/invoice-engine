47afb3d4ef10c79beb1d289884edc32ccbf6bc0c
Jacqueline <jsantillan@tiprotec.com>
2026-02-23 07:01:58 -0600

    Cambio en peso

--- invoice-backend/src/main.ts
+++ invoice-backend/src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
   app.setGlobalPrefix('api/v1');
+
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
   app.enableCors({
     origin: configService.get<string>('ALLOWED_ORIGINS')?.split(',') ?? [],
